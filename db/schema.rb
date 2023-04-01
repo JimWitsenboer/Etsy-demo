1afe5a7dce22147a1fda57e11163f05bf41024b2
Jim Witsenboer <jim.witsenboer@gmail.com>
2023-04-01 16:31:18 +0800

    added Devise to create users accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_26_104712) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_084444) do
   create_table "listings", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -22,6 +22,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_104712) do
     t.bigint "image_file_size"
     t.datetime "image_updated_at"
     t.string "image"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
